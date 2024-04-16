98ec03cf87a6170e7d7dd5f768f1c7639c269c04
MATHEUS KENJI NISHIMURA <matheuskenji12345678@gmail.com>
2024-04-16 19:57:02 -0300

    union types

--- build/aula04.js
+++ build/aula04.js
@@ -1,1 +1,9 @@
 "use strict";
+let cursos = ["Javascript", "typescript"];
+cursos.push("C++");
+let cursos2 = [];
+let valores = [10, 200.300];
+valores.push(500);
+let vteste;
+vteste = 20;
+vteste = "20";
